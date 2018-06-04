020a9913248666b2befebd58d36e64a830c106d2
nitay dolav <dolavn@cs302six1-4.cs.bgu.ac.il>
2018-06-04 19:18:00 +0300

    fixed a bug in ln.c

--- ln.c
+++ ln.c
@@ -5,11 +5,11 @@
 int
 main(int argc, char *argv[])
 {
-  if(argc != 3 || argc != 4){
+  if(argc < 3 || argc > 4){
     printf(2, "Usage: ln old new OR ls -s old new\n");
     exit();
   }
-  if (argv[1] != "-s") {
+  if (strcmp(argv[1],"-s")) {
     if (link(argv[1], argv[2]) < 0) {
       printf(2, "link %s %s: failed\n", argv[1], argv[2]);
     }
