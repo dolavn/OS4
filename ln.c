b497649f7e9b06c289cb20e6bc6b9541c9a3cfca
katz-talmon eyal <eyalkatz@cs302six1-3.cs.bgu.ac.il>
2018-06-04 19:01:51 +0300

    ln.c preparation and T_SLINK file type added

--- ln.c
+++ ln.c
@@ -5,11 +5,15 @@
 int
 main(int argc, char *argv[])
 {
-  if(argc != 3){
-    printf(2, "Usage: ln old new\n");
+  if(argc != 3 || argc != 4){
+    printf(2, "Usage: ln old new OR ls -s old new\n");
     exit();
   }
-  if(link(argv[1], argv[2]) < 0)
+  if(argv[1] != "-s" && link(argv[1], argv[2]) < 0) {
     printf(2, "link %s %s: failed\n", argv[1], argv[2]);
+    exit();
+  }
+  // Symbolic link
+  
   exit();
 }
