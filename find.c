d9cad0c6d9b8cc19a488a4f438f19cae0155e5a5
katz-talmon eyal <eyalkatz@cs302six1-3.cs.bgu.ac.il>
2018-06-08 11:54:03 +0300

    find infrastructure

---
+++ find.c
@@ -0,0 +1,16 @@
+#include "types.h"
+#include "user.h"
+
+
+
+int main(int argc, char** argv){
+
+  if(argc < 2 || argc > 4) {
+    printf(2, "Usage: find <path> [<options>] [<tests>]\n");
+    exit();
+  }
+
+  
+  // printf(2, "aaa\n");
+  exit();
+}
