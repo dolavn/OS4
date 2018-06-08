b3ce7836329e783f7e88e00e9f76256e1a9e2098
katz-talmon eyal <eyalkatz@cs302six1-3.cs.bgu.ac.il>
2018-06-08 13:15:39 +0300

    find's search

--- find.c
+++ find.c
@@ -1,5 +1,7 @@
 #include "types.h"
+#include "stat.h"
 #include "user.h"
+#include "fcntl.h"
 
 struct search_criteria{
   char* path;
@@ -12,10 +14,9 @@ struct search_criteria{
 };
 
 struct search_criteria search_crit;
-
-void get_search_criteria(int, char**);
-
+int get_search_criteria(int, char**);
 void search(struct search_criteria*);
+int checkTests(struct search_criteria*, struct stat*);
 
 int main(int argc, char** argv){
 
@@ -24,6 +25,59 @@ int main(int argc, char** argv){
     exit();
   }
 
-  
+
   exit();
+}
+
+void
+search(struct search_criteria* criteria) {
+  int fd;
+  struct stat st;
+  char* path = criteria->path;
+
+  if((fd = open(path, criteria->follow?O_RDONLY:O_IGN_SLINK)) < 0) {
+    printf(2, "find: cannot open %s\n", path);
+    return;
+  }
+  if(fstat(fd, &st) < 0) {
+    printf(2, "find: cannot stat %s\n", path);
+    close(fd);
+    return;
+  }
+
+  switch(st.type) {
+  case T_FILE:
+    if(checkTests(criteria, &st)) {
+      printf(1, "%s\n", path);
+    }
+    break;
+
+  // case T_DIR:
+  //   if(strlen(path) + 1 + DIRSIZ + 1 > sizeof buf){
+  //     printf(1, "ls: path too long\n");
+  //     break;
+  //   }
+  //   strcpy(buf, path);
+  //   p = buf+strlen(buf);
+  //   *p++ = '/';
+  //   while(read(fd, &de, sizeof(de)) == sizeof(de)){
+  //     if(de.inum == 0)
+  //       continue;
+  //     memmove(p, de.name, DIRSIZ);
+  //     p[DIRSIZ] = 0;
+  //     if(stat(buf, &st) < 0){
+  //       printf(1, "ls: cannot stat %s\n", buf);
+  //       continue;
+  //     }
+  //     printf(1, "%s %d %d %d\n", fmtname(buf), st.type, st.ino, st.size);
+  //   }
+  //   break;
+  }
+
+  close(fd);
+}
+
+int checkTests(struct search_criteria* sc, struct stat* st) {
+  /* code */
+  return 0;
 }
