63acbbf6068617066ed2dda7255f04d3a1a84f15
katz-talmon eyal <eyalkatz@cs302six1-3.cs.bgu.ac.il>
2018-06-08 13:22:36 +0300

    Merge branch 'master' of https://github.com/dolavn/OS4

--- find.c
+++ find.c
@@ -19,13 +19,11 @@ void search(struct search_criteria*);
 int checkTests(struct search_criteria*, struct stat*);
 
 int main(int argc, char** argv){
-
-  if(argc < 2) {
+  if(get_search_criteria(argc, argv) < 0) {
     printf(2, "Usage: find <path> [<options>] [<tests>]\n");
     exit();
   }
-
-
+  //printf(2,"search criteria:\n%s\nfollow:%d\nname:%s\nsize:%d\ntype:%d\nkey:%s\nvalue:%s\n",search_crit.path,search_crit.follow,search_crit.name,search_crit.size,search_crit.type,search_crit.key,search_crit.value);
   exit();
 }
 
@@ -79,5 +77,68 @@ search(struct search_criteria* criteria) {
 
 int checkTests(struct search_criteria* sc, struct stat* st) {
   /* code */
+}
+
+int get_search_criteria(int argc, char** argv){
+  if(argc<2){return -1;}
+  search_crit.path = argv[1];
+  search_crit.follow = 0;
+  search_crit.name = 0;
+  search_crit.size = 0;
+  search_crit.type = 0;
+  search_crit.key = 0;
+  search_crit.value = 0;
+  for(int i=2;i<argc;++i){
+    if(strcmp(argv[i],"-follow")==0){
+      search_crit.follow=1;
+      continue;
+    }
+    if(strcmp(argv[i],"-name")==0){
+      if(i==argc-1){return -1;}
+      search_crit.name = argv[i+1];
+      i = i + 1;
+      continue;
+    }
+    if(strcmp(argv[i],"-size")==0){
+      if(i==argc-1){return -1;}
+      if(argv[i+1][0]!='+' && argv[i+1][0]!='-'){
+        return -1;
+      }
+      search_crit.size = (argv[i+1][0]=='+'?1:-1)*(atoi(argv[i+1]+1));
+      i = i + 1;
+      continue;
+    }
+    if(strcmp(argv[i],"-type")==0){
+      if(i==argc-1){return -1;}
+      if(strcmp(argv[i+1],"d")==0){
+        search_crit.type = T_DIR;
+        i = i + 1;
+        continue;
+      }
+      if(strcmp(argv[i+1],"f")==0){
+        search_crit.type = T_FILE;
+        i = i + 1;
+        continue;
+      }
+      if(strcmp(argv[i+1],"s")==0){
+        search_crit.type = T_SLINK;
+        i = i + 1;
+        continue;
+      }
+      return -1;
+    }
+    if(strcmp(argv[i],"-tag")==0){
+      if(i==argc-1){return -1;}
+      for(char* value=argv[i+1];*value;value++){
+        if(*value=='='){
+          *value = 0;
+          search_crit.value = value+1;
+          break;
+        }
+      }
+      if(!search_crit.value){return -1;}
+      search_crit.key = argv[i+1];
+    }
+  }
   return 0;
 }
