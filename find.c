5bfd5cee19e959da6f8693ce631db87f52673d15
nitay dolav <dolavn@cs302six1-4.cs.bgu.ac.il>
2018-06-08 12:32:15 +0300

    added declarations find.c

--- find.c
+++ find.c
@@ -1,16 +1,29 @@
 #include "types.h"
 #include "user.h"
 
+struct search_criteria{
+  char* path;
+  char follow;
+  char* name;
+  int size;
+  char type;
+  char* key;
+  char* value;
+};
 
+struct search_criteria search_crit;
+
+void get_search_criteria(int, char**);
+
+void search(struct search_criteria*);
 
 int main(int argc, char** argv){
 
-  if(argc < 2 || argc > 4) {
+  if(argc < 2) {
     printf(2, "Usage: find <path> [<options>] [<tests>]\n");
     exit();
   }
 
   
-  // printf(2, "aaa\n");
   exit();
 }
